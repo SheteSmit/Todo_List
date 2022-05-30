b4d60a7896604d6b5cd8d3509fbd08e1a836c8b9
SheteSmit <shetesmit@gmail.com>
2022-05-30 13:33:20 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef, useEffect } from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid'
+const localStorageKey = 'todoApp.todos'
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+  
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(localStorageKey))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id) 
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') {return}
+    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, {id:uuidv4(), name:name, complete:false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <TodoList todos = {todos} toggleTodo = {toggleTodo}/>
+      <input ref={todoNameRef} type ="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Completed Todo</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+
+    </>
+  )
+}
+
+export default App;
